3642fa85b8c54c1e524e8ce7a183a2d0ae036ac8
eisWaldbronn <eis.waldbronn@gmail.com>
2020-11-25 12:23:18 +0100

    no mobile scripts

--- assets/ts/main-nav.ts
+++ assets/ts/main-nav.ts
@@ -1,14 +1,14 @@
-const NAV_HIDDEN_CLASS = 'nav--mobile-hidden';
-const NAV_MAIN = document.querySelector<HTMLElement>('header .nav__main');
+// const NAV_HIDDEN_CLASS = 'nav--mobile-hidden';
+// const NAV_MAIN = document.querySelector<HTMLElement>('header .nav__main');
 
-document.querySelector('.nav__mobile').addEventListener('click', ev => {
-    NAV_MAIN.classList.toggle(NAV_HIDDEN_CLASS);
-    ev.preventDefault();
-    ev.stopPropagation();
-});
+// document.querySelector('.nav__mobile').addEventListener('click', ev => {
+//     NAV_MAIN.classList.toggle(NAV_HIDDEN_CLASS);
+//     ev.preventDefault();
+//     ev.stopPropagation();
+// });
 
-window.addEventListener('click', () => {
-    if (!NAV_MAIN.classList.contains(NAV_HIDDEN_CLASS)) {
-        NAV_MAIN.classList.add(NAV_HIDDEN_CLASS);
-    }
-});
+// window.addEventListener('click', () => {
+//     if (!NAV_MAIN.classList.contains(NAV_HIDDEN_CLASS)) {
+//         NAV_MAIN.classList.add(NAV_HIDDEN_CLASS);
+//     }
+// });
